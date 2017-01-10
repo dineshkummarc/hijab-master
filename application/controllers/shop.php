ef455c0ad6c13a3a48b5d421d39f044b3c414d7b
Deni Pratama S <deni@komunigrafik.com>
2017-01-10 21:32:47 +0700

    bug

--- application/controllers/shop.php
+++ application/controllers/shop.php
@@ -15,7 +15,6 @@ class Shop extends PX_Controller {
 		$data = $this->get_app_settings();
 		$data += $this->controller_attr;
 		$data += $this->get_function('Shop','shop');
-        die(print_r($this->session->userdata('id')));
 		$data['address']= $this->model_basic->select_where($this->tbl_static_content,'id','6')->row();
 		$data['phone']= $this->model_basic->select_where($this->tbl_static_content,'id','7')->row();
 		$data['fax']= $this->model_basic->select_where($this->tbl_static_content,'id','8')->row();
